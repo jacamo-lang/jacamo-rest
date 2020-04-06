220d0e4a8212082b1aa075f6d71b479dfff154b1
Jomi Fred Hubner <jomifred@gmail.com>
2020-04-06 12:26:21 -0300

    use new jason api for runtimeservices

--- src/main/java/jacamo/rest/JCMRest.java
+++ src/main/java/jacamo/rest/JCMRest.java
@@ -6,6 +6,13 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.core.UriBuilder;
 
@@ -13,11 +20,13 @@ import org.apache.commons.io.FileUtils;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.x.async.WatchMode;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
+import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.server.NIOServerCnxnFactory;
 import org.apache.zookeeper.server.ServerCnxnFactory;
 import org.apache.zookeeper.server.ZooKeeperServer;
@@ -25,7 +34,17 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 
 import jacamo.platform.DefaultPlatformImpl;
+import jason.architecture.AgArch;
+import jason.asSemantics.Unifier;
+import jason.asSyntax.ASSyntax;
+import jason.asSyntax.Atom;
+import jason.asSyntax.Literal;
+import jason.asSyntax.StringTermImpl;
+import jason.asSyntax.Term;
+import jason.asSyntax.UnnamedVar;
 import jason.infra.centralised.BaseCentralisedMAS;
+import jason.runtime.DelegatedRuntimeServices;
+import jason.runtime.RuntimeServicesFactory;
 
 
 public class JCMRest extends DefaultPlatformImpl {
@@ -59,8 +78,11 @@ public class JCMRest extends DefaultPlatformImpl {
     @Override
     public void init(String[] args) throws Exception {
         
+        // change the runtimeservices
+        RuntimeServicesFactory.set( new JCMRuntimeServices() );
+        
         // adds RestAgArch as default ag arch when using this platform
-        BaseCentralisedMAS.getRunner().getRuntimeServices().registerDefaultAgArch(RestAgArch.class.getName());
+        RuntimeServicesFactory.get().registerDefaultAgArch(RestAgArch.class.getName());
         
         int restPort = 3280;
         int zkPort   = 2181;
@@ -113,6 +135,21 @@ public class JCMRest extends DefaultPlatformImpl {
             }
         }
         
+    }
+
+    Map<String, RestAgArch> archCache = new HashMap<>();
+    
+    protected RestAgArch getRestAgArch(String agName) {
+        return archCache.computeIfAbsent(agName, k -> {
+            AgArch arch = BaseCentralisedMAS.getRunner().getAg(agName).getFirstAgArch();
+            while (arch != null) {
+                if (arch instanceof RestAgArch) {
+                    return (RestAgArch)arch;
+                }
+                arch = arch.getNextAgArch();
+            }
+            return null;
+        });
     }
     
     @Override
@@ -196,7 +233,7 @@ public class JCMRest extends DefaultPlatformImpl {
         int tickTime = 2000;
 
         try {
-            cleanZK();
+            cleanZKFiles();
             
             zkHost = InetAddress.getLocalHost().getHostAddress()+":"+port;
 
@@ -225,7 +262,7 @@ public class JCMRest extends DefaultPlatformImpl {
         }
     }
     
-    void cleanZK() {
+    void cleanZKFiles() {
         for (File f: FileUtils.getTempDirectory().listFiles()) {
             if (f.getName().startsWith(zkTmpFileName)) {
                 try {
@@ -236,11 +273,124 @@ public class JCMRest extends DefaultPlatformImpl {
         }
     }
 
-    static CuratorFramework getZKClient() {
+    public static CuratorFramework getZKClient() {
         if (zkClient == null) {
             zkClient = CuratorFrameworkFactory.newClient(getZKHost(), new ExponentialBackoffRetry(1000, 3));
             zkClient.start();
         }
         return zkClient;
     }
+    
+    class JCMRuntimeServices extends DelegatedRuntimeServices {
+        public JCMRuntimeServices() {
+            super(RuntimeServicesFactory.get());
+        }
+
+        @Override
+        public void dfRegister(String agName, String service, String type) {
+            try {
+                if (type == null) type = "no-type";
+                String node = JCMRest.JaCaMoZKDFNodeId+"/"+service+"/"+agName;
+                if (getZKClient().checkExists().forPath(node) == null) {
+                    getZKClient().create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(node, type.getBytes());
+                } else {
+                    getZKClient().setData().forPath(node, type.getBytes());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        @Override
+        public void dfDeRegister(String agName, String service, String type) {
+            try {
+                getZKClient().delete().forPath(JCMRest.JaCaMoZKDFNodeId+"/"+service+"/"+agName);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        @Override
+        public Collection<String> dfSearch(String service, String type) {
+            Set<String> ags = new HashSet<>();
+            try {
+                if (getZKClient().checkExists().forPath(JCMRest.JaCaMoZKDFNodeId+"/"+service) != null) {
+                    for (String r : getZKClient().getChildren().forPath(JCMRest.JaCaMoZKDFNodeId+"/"+service)) {
+                        ags.add(r);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return ags;
+        }
+        
+        @Override
+        public void dfSubscribe(String agName, String service, String type) {
+            try {
+                RestAgArch arch = getRestAgArch(agName); 
+                arch.getAsyncCurator()
+                    .with(WatchMode.successOnly).watched().getChildren().forPath(JCMRest.JaCaMoZKDFNodeId+"/"+service).event().thenAccept(event -> {
+                        try {
+                            //System.out.println("something changed...."+event.getType()+"/"+event.getState());
+                            // stupid implementation: send them all again and
+                            dfSubscribe(agName, service, type); // keep watching
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+                // update providers
+                Term s = new Atom("df");
+                Literal l = ASSyntax.createLiteral("provider", new UnnamedVar(), new StringTermImpl(service));
+                l.addSource(s);
+                arch.getTS().getAg().abolish(l, new Unifier());
+                for (String a: dfSearch(service, type)) {
+                    l = ASSyntax.createLiteral("provider", new Atom(a), new StringTermImpl(service));
+                    l.addSource(s);
+                    arch.getTS().getAg().addBel(l);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        
+        @Override
+        public Collection<String> getAgentsNames() {
+            // use ZK WP
+            try {
+                List<String> all = new ArrayList<>();
+                for (String ag : getZKClient().getChildren().forPath(JCMRest.JaCaMoZKAgNodeId)) {
+                    all.add(ag);
+                }
+                return all;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return super.getAgentsNames();
+            }
+        }
+        
+        @Override
+        public Map<String, Set<String>> getDF() {
+            if (getZKHost() == null) {
+                return super.getDF();
+            } else {
+                try {
+                    Map<String, Set<String>> commonDF = new HashMap<String, Set<String>>();
+
+                    for (String s : getZKClient().getChildren().forPath(JCMRest.JaCaMoZKDFNodeId)) {
+                        for (String a : getZKClient().getChildren().forPath(JCMRest.JaCaMoZKDFNodeId + "/" + s)) {
+                            commonDF.computeIfAbsent(a, k -> new HashSet<>()).add(s);
+                        }
+                    }
+                    return commonDF;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        }                   
+    }
+
 }
