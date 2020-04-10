bc67cdb73c9ae08f924ee8909800ea6f7c2d544e
Timotheus Kampik <timotheus.kampik@gmail.com>
2020-04-10 15:52:43 +0200

    remove unused imports

--- src/test/java/jacamo/rest/ClientAgentTest.java
+++ src/test/java/jacamo/rest/ClientAgentTest.java
@@ -16,7 +16,6 @@ import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
--- src/test/java/jacamo/rest/ClientOverviewTest.java
+++ src/test/java/jacamo/rest/ClientOverviewTest.java
@@ -9,7 +9,6 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
