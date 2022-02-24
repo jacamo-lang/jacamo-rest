df656d103790d11016a573348f12e5246283e55c
Jomi Fred Hubner <jomifred@gmail.com>
2022-02-24 12:40:10 -0300

    update rest doc

--- scripts/generate-rest-doc.sh
+++ scripts/generate-rest-doc.sh
@@ -2,11 +2,16 @@
 
 ./gradlew docSwagger
 
+rm -rf doc/rest-api
+mkdir doc/rest-api
+
 docker run --rm -v $(pwd):/opt swagger2markup/swagger2markup convert -i \
    /opt/build/generated/swagger-ui/swagger.json \
-   -f /opt/doc/rest-api \
+   -f /opt/doc/rest-api/swagger \
    -c /opt/scripts/swagger2markup.properties
 
-docker run --rm -it -v $(pwd)/doc:/documents asciidoctor/docker-asciidoctor \
-    asciidoctor rest-api.adoc
+docker run --rm -it -v $(pwd)/doc/rest-api:/documents asciidoctor/docker-asciidoctor \
+    asciidoctor swagger.adoc
+
+cp doc/rest-api/swagger.html readme.html
 #/documents/doc/rest-api.adoc
