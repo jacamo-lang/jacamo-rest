86bae6bbb6108b62c0b38a07ff20ab25273cc987
Jomi Fred Hubner <jomifred@gmail.com>
2022-08-15 09:49:04 -0300

    Update demo-art.py

--- demos/python-jacamo/demo-art.py
+++ demos/python-jacamo/demo-art.py
@@ -11,6 +11,7 @@ count_url = "http://127.0.0.1:8080/workspaces/w/artifacts/c1/properties/count"
 response = requests.get(count_url)
 print("Count value is ",response.json()[0])
 
+# update the value of an observable property
 msg     = [ 13 ]
 headers = {"Content-Type":"application/json"}
 response = requests.post(count_url, data=json.dumps(msg), headers=headers)
